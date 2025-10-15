0bca15d79675845d939daf3b424fb3456cda1610
Marino3005 <marino.ramon@usal.edu.ar>
2025-10-15 13:12:50 -0300

    create(user domain class)

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,11 @@
 // TODO:
+export class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  getInfo() {
+    return `Name: ${this.name}, Email: ${this.email}`;
+  }
+}
