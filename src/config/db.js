54214230229b3a0446a866c4b790bef309eaa4b5
Marino3005 <marino.ramon@usal.edu.ar>
2025-10-17 08:32:25 -0300

    remove logs

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config({ quiet: true });
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD, process.env.DB_USER);
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "postgres",
